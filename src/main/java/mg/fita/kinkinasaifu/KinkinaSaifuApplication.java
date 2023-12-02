e9cdd133b92132cde95ce0885f883c65f6133122
MiiJack <f.rama723@gmail.com>
2023-12-02 16:42:07 +0300

    initial commit

---
+++ src/main/java/mg/fita/kinkinasaifu/KinkinaSaifuApplication.java
@@ -0,0 +1,13 @@
+package mg.fita.kinkinasaifu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinkinaSaifuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinkinaSaifuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/fita/kinkinasaifu/KinkinaSaifuApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.fita.kinkinasaifu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinkinaSaifuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
