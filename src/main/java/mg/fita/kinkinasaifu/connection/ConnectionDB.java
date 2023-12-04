3543445905405cbb6d3073705ea32cce1415fc63
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-12-04 22:31:12 +0300

    feat: add connection to database

---
+++ src/main/java/mg/fita/kinkinasaifu/connection/ConnectionDB.java
@@ -0,0 +1,29 @@
+package mg.fita.kinkinasaifu.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    private static Connection connection;
+
+    private ConnectionDB(){
+        String url = System.getenv("DATABASE_URL");
+        String user = System.getenv("DATABASE_USER");
+        String password = System.getenv("DATABASE_PASSWORD");
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection successfull !");
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the PostgreSQL database: " + e.getMessage());
+        }
+    }
+    public static Connection getConnection(){
+        if (connection == null) {
+            new ConnectionDB();
+        }
+        return connection;
+    }
+}
