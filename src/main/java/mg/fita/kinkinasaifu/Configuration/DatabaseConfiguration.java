dbc42cf722d39437d6c2ccc3b99d16aaba21cea4
MiiJack <f.rama723@gmail.com>
2023-12-04 23:41:54 +0300

    feat: added connection to the database

---
+++ src/main/java/mg/fita/kinkinasaifu/Configuration/DatabaseConfiguration.java
@@ -0,0 +1,24 @@
+package mg.fita.kinkinasaifu.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfiguration {
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        try{
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost/library_management",
+                    System.getenv("DB_USER"),
+                    System.getenv("DB_PASSWORD")
+            );
+            return connection;
+        }
+        catch (SQLException error){
+            System.out.println(error.getMessage());
+            throw new RuntimeException("Connection failed");
+        }
+    }
+}
