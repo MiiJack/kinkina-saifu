b5947a92f98f3ca80e04c4dd86956e8504fce61e
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-12-08 00:02:58 +0300

    chore: try to test in the main code

--- src/main/java/mg/fita/kinkinasaifu/Main.java
+++ src/main/java/mg/fita/kinkinasaifu/Main.java
@@ -4,25 +4,37 @@ import mg.fita.kinkinasaifu.connection.ConnectionDB;
 import mg.fita.kinkinasaifu.repositories.*;
 import mg.fita.kinkinasaifu.model.*;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         ConnectionDB.getConnection();
-        // Create mock data
-        Currency currency = new Currency(1, "USD", "Dollar");
-        Account account = new Account(7, currency, "Test Account");
-        Transaction transaction = new Transaction(1, account, BigDecimal.valueOf(1000), LocalDateTime.now());
 
         // Create repository objects
         AccountCrudOperations accountCrudOperations = new AccountCrudOperations();
         TransactionCrudOperations transactionCrudOperations = new TransactionCrudOperations();
+        OtherCrudOperations otherCrudOperations = new OtherCrudOperations();
+
+        // Create mock data
+        Currency currency6 = otherCrudOperations.findById(6);
+        Currency currency5 = otherCrudOperations.findById(5);
+        Balance balance1 = new Balance(3500.00, LocalDateTime.now());
+        Balance balance2 = new Balance(200.00, LocalDateTime.now());
+        Transaction transaction1 = new Transaction(6, "Testing N1", 1000.00, LocalDateTime.now(),
+                "Debit", null, null);
+        Account account1 = new Account(6, "Savings", balance1, List.of(transaction1), currency6, "Bank");
+        Transaction transaction2 = new Transaction(7, "Testing N2", 700.00, LocalDateTime.now(),
+                "Credit", null, null);
+        Account account2 = new Account(7, "Current", balance2, List.of(transaction2,transaction1), currency5, "Cash");
 
         // Test save methods
-        accountCrudOperations.save(account);
-        transactionCrudOperations.save(transaction);
+        accountCrudOperations.save(account1);
+        transactionCrudOperations.save(transaction1);
+        accountCrudOperations.save(account2);
+        transactionCrudOperations.save(transaction2);
+        otherCrudOperations.saveBalance(balance1,account1.getId());
+        otherCrudOperations.saveBalance(balance2,account2.getId());
 
         // Test findAll methods
         List<Account> allAccounts = accountCrudOperations.findAll();
