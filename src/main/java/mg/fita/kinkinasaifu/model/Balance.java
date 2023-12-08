727b82cc33e44c349a34dae781fb07bdfe44221f
MiiJack <f.rama723@gmail.com>
2023-12-08 16:52:46 +0300

    chore: cleanup the models

--- src/main/java/mg/fita/kinkinasaifu/model/Balance.java
+++ src/main/java/mg/fita/kinkinasaifu/model/Balance.java
@@ -1,16 +1,12 @@
 package mg.fita.kinkinasaifu.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
 public class Balance {
     private Double value;
     private LocalDateTime modificationDate;
