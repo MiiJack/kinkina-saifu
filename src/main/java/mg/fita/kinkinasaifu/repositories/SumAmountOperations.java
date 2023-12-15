aec91b391724612a0ab3308f7f440bf80381f5a3
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-12-15 14:49:57 +0300

    feat: calculateMappedSumAmounts operation

--- src/main/java/mg/fita/kinkinasaifu/repositories/SumAmountOperations.java
+++ src/main/java/mg/fita/kinkinasaifu/repositories/SumAmountOperations.java
@@ -4,6 +4,7 @@ import mg.fita.kinkinasaifu.connection.ConnectionDB;
 import mg.fita.kinkinasaifu.model.SumAmount;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,5 +43,52 @@ public class SumAmountOperations {
         }
 
         return sumAmounts;
+    }
+
+        public List<SumAmount> calculateMappedSumAmounts(int accountId, LocalDateTime startDate, LocalDateTime endDate) {
+        List<SumAmount> sumAmounts = new ArrayList<>();
+
+        String TRANSACTIONS_BY_DATE_QUERY = "SELECT label, amount, date_time, type FROM transaction " +
+                "JOIN account_transaction ON transaction.id = account_transaction.transaction_id " +
+                "WHERE account_transaction.account_id = ? AND date_time BETWEEN ? AND ?";
+        try (PreparedStatement statement = connection.prepareStatement(TRANSACTIONS_BY_DATE_QUERY)) {
+            statement.setInt(1, accountId);
+            statement.setTimestamp(2, Timestamp.valueOf(startDate));
+            statement.setTimestamp(3, Timestamp.valueOf(endDate));
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    String category = resultSet.getString("label");
+                    double amount = resultSet.getDouble("amount");
+                    String type = resultSet.getString("type");
+
+                    if ("Credit".equals(type)) {
+                        amount = -amount; // Convert Credit to negative amount
+                    }
+
+                    SumAmount sumAmount = new SumAmount(category, amount);
+                    sumAmounts.add(sumAmount);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Map data and calculate total amounts for each category
+        List<SumAmount> mappedSumAmounts = new ArrayList<>();
+        for (SumAmount sumAmount : sumAmounts) {
+            String category = sumAmount.getCategory();
+            double totalAmount = sumAmounts.stream()
+                    .filter(sa -> sa.getCategory().equals(category))
+                    .mapToDouble(SumAmount::getTotalAmount)
+                    .sum();
+
+            SumAmount mappedSumAmount = new SumAmount(category, totalAmount);
+            if (!mappedSumAmounts.contains(mappedSumAmount)) {
+                mappedSumAmounts.add(mappedSumAmount);
+            }
+        }
+
+        return mappedSumAmounts;
     }
 }
