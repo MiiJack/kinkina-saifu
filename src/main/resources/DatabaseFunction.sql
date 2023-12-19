3a3255b33601cf9ecc20dadb1a6cf59b0d171713
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-12-19 07:24:43 +0300

    fix(sql): calculate_category_sums

--- src/main/resources/DatabaseFunction.sql
+++ src/main/resources/DatabaseFunction.sql
@@ -1,30 +1,3 @@
-CREATE FUNCTION SumCategories(
-   account_id INT,
-   start_date TIMESTAMP,
-   end_date TIMESTAMP) 
-RETURNS TABLE (category VARCHAR(255), total_amount DOUBLE PRECISION) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT
-        category,
-        COALESCE(SUM(CASE WHEN t.type = 'Credit' THEN t.amount ELSE -t.amount END), 0) AS total_amount
-    FROM (
-        SELECT DISTINCT label AS category
-        FROM "transaction"
-    ) categories
-    LEFT JOIN "transaction" t ON categories.category = t.label
-    AND t.date_time BETWEEN start_date AND end_date
-    AND EXISTS (
-        SELECT 1
-        FROM "account_transaction" at
-        WHERE at.account_id = account_id
-        AND at.transaction_id = t.id
-    )
-    GROUP BY category;
-    RETURN;
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION calculate_money_in_out(
   account_id_param INT,
   start_date TIMESTAMP,
@@ -43,6 +16,30 @@ BEGIN
       JOIN account_transaction ON transaction.id = account_transaction.transaction_id
       WHERE account_transaction.account_id = account_id_param
       AND transaction.date_time BETWEEN start_date AND end_date
+  );
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION calculate_category_sums(
+  account_id_param INT,
+  start_date TIMESTAMP,
+  end_date TIMESTAMP
+)
+RETURNS TABLE(category VARCHAR, total DOUBLE PRECISION) AS $$
+BEGIN
+  RETURN QUERY (
+      SELECT transaction.category, 
+      SUM(
+          CASE 
+              WHEN transaction.category IS NULL THEN 0
+              ELSE transaction.amount
+          END
+      ) AS total
+      FROM transaction
+      LEFT JOIN account_transaction ON transaction.id = account_transaction.transaction_id
+      WHERE account_transaction.account_id = account_id_param
+      AND transaction.date_time BETWEEN start_date AND end_date
+      GROUP BY transaction.category
   );
 END;
 $$ LANGUAGE plpgsql;
